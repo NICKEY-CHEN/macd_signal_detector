d05f46d3049673bdd86bf03908815fa4bb07d871
NICKEY-CHEN <32220638+NICKEY-CHEN@users.noreply.github.com>
2021-02-25 13:47:23 +0800

    Update quant_estimate_market.py

--- quant_estimate_market.py
+++ quant_estimate_market.py
@@ -59,7 +59,7 @@ class estimate_market():
 
 
 # if __name__ == '__main__':
-#     ts.set_token('b1e513696c465675d9b83754bef0e630987c989ffbc8b48779551c99')
+#     ts.set_token('')
 #     pro = ts.pro_api()
 #     code = '002340.SZ'
 #     begin_date = '20200709'
